dfb7a2dbe84adc21b3c7e97fc14c62abfe06e0fa
Emirhan Korkmaz <82825056+emirhankorkmaz@users.noreply.github.com>
2023-11-12 16:27:57 +0300

    Delete src/Row.js

--- src/Row.js
+++
@@ -1,8 +0,0 @@
-// src/utils/imageLoader.js
-
-const selectedImage1 = require('./contents/images/Rectangle 1.jpg');
-const selectedImage2 = require('./contents/images/Rectangle 2.jpg');
-const selectedImage3 = require('./contents/images/Rectangle 3.jpg');
-const selectedImage4 = require('./contents/images/Rectangle 4.jpg');
-
-export default [selectedImage1, selectedImage2, selectedImage3, selectedImage4];
